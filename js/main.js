126a105fc19b4ae429e0e2cc48dc7c12087849bd
lordevito <lollo.devito@hotmail.com>
2023-12-05 18:45:08 +0100

    feat: esercizio scritto da fixare

--- js/main.js
+++ js/main.js
@@ -1,2 +1,23 @@
 `use strict`;
 
+const { createApp, ref } = Vue;
+
+  createApp({
+    setup() {
+      const message = ref('Hello vue!')
+      return {
+            emailList: [],
+      };
+    },
+    methods: {},
+    mounted: function(){
+        for(let i = 0; i < 10; i++) {
+            axios
+            .get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((result)=> {
+                this.email = result.data.response;
+                this.emailList.push(this.email);
+            });
+        };
+    },
+  }).mount('#app');
